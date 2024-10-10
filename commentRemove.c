3709e606bc8bb47f873498a02d3ab6d8bba65f3b
tonyhdz24 <tonyhernandez24@u.boisestate.edu>
2024-10-09 20:34:40 -0600

    Ignores Comments but does not produce an new Ouput.c file

--- commentRemove.c
+++ commentRemove.c
@@ -1,8 +1,58 @@
 #include <stdio.h>
+#include <string.h>
+
+void readFile(char prg[]);
+void readLine(char line[]);
 
 int main(int argc, char const *argv[]) {
-  // 1. Take in a C file
-  // 2. Copy the content of that file to a new file
-  // 3.
+  char test[100] = "./helloWorld.c";
+  readFile(test);
   return 0;
+}
+void readLine(char line[]) {
+  // Variable to keep track if scanner is in comment or not
+  int inComment = 1;
+  int outComment = 0;
+  int state = outComment;
+
+  int i, c;
+  int lineLength = strlen(line);
+
+  for (i = 0; i < lineLength; i++) {
+    c = line[i];
+    // check if scanner is at the end of a line
+    // if (c == '\n') {
+    //   printf("__ %d", i);
+    // }
+
+    // Check IF scanner has encountred a comment character //
+    if (c == '/') {
+      state = inComment;
+      // printf("%c", c);
+    }
+
+    if (c != '/' && state == outComment) {
+      printf("%c", c);
+    }
+  }
+};
+
+void readFile(char prg[]) {
+  FILE *fptr;
+
+  // open a file in read mode
+  fptr = fopen(prg, "r");
+  // Store the content of the file
+  char prgString[100];
+  // If the file exists
+  if (fptr != NULL) {
+    // Read content and print it
+    while (fgets(prgString, 100, fptr)) {
+      // Pass each line from the file into line reader
+      readLine(prgString);
+    }
+  } else {
+    printf("Not able to open the file.");
+  }
+  fclose(fptr);
 }
---
+++ helloWorld.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  // My first C program
+  printf("Hello World!!"); // A comment next to printf
+  return 0;
+}
